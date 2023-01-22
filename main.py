96884e977856d90e37ec43119a7fe09b9122b935
mihailermak <122021329+mihailermak@users.noreply.github.com>
2023-01-22 17:57:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from currency_converter import CurrencyConverter
 from tkinter import *
+
+#обозначаем Tkinter за window 
+window = Tk()
+
+#обозначаем CurrencyConverter за converter
+converter = CurrencyConverter
+
+#задаем заголовок окна
+window.title('RUB-USD-EUR-TRY-converter')
+
+#задаем размеры окна
+window.geometry('400x300')
+
+#фиксируем значения размера окна
+window.resizable(width=False, height=False)
+
+#добавляем цвет фона
+window['bg'] = 'white'
+
